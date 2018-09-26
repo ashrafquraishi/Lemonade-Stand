b4ec1856b1c56bd0ad99086c31849f7a22a3b295
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-09-26 08:19:21 -0500

    Lemonade-stand introduction

---
+++ LmS/LmS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LmS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Introduction introduction = new Introduction();
+            List<string> results = introduction.AddIntroduction();
+            introduction.DisplayIntroduction(results);
+
+            Console.ReadLine();
+        }
+    }
+}
