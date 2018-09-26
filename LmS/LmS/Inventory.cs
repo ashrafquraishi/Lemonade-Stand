1b1f4800bac8253965d85477a99b3ec493df08fd
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-09-26 10:45:38 -0500

    made member variables for inventory, also assigned their prices

--- LmS/LmS/Inventory.cs
+++ LmS/LmS/Inventory.cs
@@ -9,5 +9,29 @@ namespace LmS
     class Inventory
     {
         //Inventory and their pricing
+        // member variables
+        //
+        public Double LemonPrice = 0.50;
+        public int Lemons;
+        // SugarCup= 6 cups of Lemonade
+        public Double Sugar = 0.40;
+        public int SugarCup;
+        public Double Ice = 0.70;
+        public int IceCubes;
+        public Double PaperCups = 0.65;
+        public int CupsQuantity; 
+        public int Budget = 20;
+
+        public void IngredientsQuantity()
+        {
+            SugarCup = 2;
+            IceCubes = 100;
+            CupsQuantity = 10;
+            Lemons = 6;
+
+        }
+
+
     }
+
 }
