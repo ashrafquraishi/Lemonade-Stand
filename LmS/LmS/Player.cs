92d8c601fa40f9014dba73753ab6dda21e5986d1
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-01 19:45:50 -0500

    Made method for player to make recipe of his own

--- LmS/LmS/Player.cs
+++ LmS/LmS/Player.cs
@@ -10,15 +10,15 @@ namespace LmS
     {
 
         public double wallet = 20;
-       
-        public string PlayerInventory;
+
+        public int Jug = 12;
         public double Recipe;
         public double DayProfit;
         public double DayLoss;
-        public int Sugar;
-        public int Icecubes;
-        public int PaperCups;
-        public int lemon;
+        public int SugarRecipe;
+        public int IceRecipe;
+        public int LemonRecipe;
+        public double LemonadePrice;
         public void WalletExpenses(double demandCost)
         { 
             wallet -= demandCost;
@@ -28,15 +28,55 @@ namespace LmS
 
 
         }
-       static public void FillingInventory(int SugarInventory)
+
+        public void FillingSugarInventory(int SugarInventory)
         {
-            Console.WriteLine("Sugarbags=",SugarInventory);
-          
-           
+            Console.WriteLine("Sugarbags=", SugarInventory);
+            SugarInventory = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        public void FillingLemonInventory(int LemonInventory)
+        {
+            Console.WriteLine("Lemons=", LemonInventory);
+            LemonInventory = Convert.ToInt32(Console.ReadLine());
+
 
         }
 
+        public void FillingIceInventory(int IceInventory)
+        {
+            Console.WriteLine("Ice=", IceInventory);
+           IceInventory = Convert.ToInt32(Console.ReadLine());
 
+
+        }
+        public void FillingCupsInventory(int CupsInventory)
+        {
+            Console.WriteLine("Cups=", CupsInventory);
+            CupsInventory = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+        public void PreparingRecipe()
+        {
+            Console.WriteLine("Please Enter the price of one Lemonade Cup");
+           LemonadePrice= Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+
+            Console.WriteLine("Please Enter How many Sugarbags would like to use per Jug= ",Jug);
+            SugarRecipe= Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Please Enter how many IceCubes would like to use per Cup");
+             IceRecipe= Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Please Enter how many Lemon would like to use per jug=", Jug);
+
+        }
+        
     }
      
 }
